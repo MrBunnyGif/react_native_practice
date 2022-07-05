1ecb726ef3c660f2fe5f9f06613646e96f9750e0
Pedro Botega <botegapedro@gmail.com>
2022-07-04 21:53:03 -0300

    Image tests

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
-
+  console.log(require('./assets/favicon.png'))
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
+      <Text style={stylesText.container}>Hello world!</Text>
+      <Image source={{
+        width: 200,
+        height: 300,
+        uri: 'https://picsum.photos/200/300',
+      }}
+        // blurRadius={10}
+        fadeDuration={1000}
+      />
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -14,8 +22,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#333',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+});
+
+const stylesText = StyleSheet.create({
+  container: {
+    color: 'white'
   },
 });
